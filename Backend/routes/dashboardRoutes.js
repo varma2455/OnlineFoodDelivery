2360f25e52541fe9b52ee86b13f12d6bfd81cffe
Yeswanth Varma <your_email@example.com>
2026-07-17 22:43:51 +0530

    ADDING VALUES2

--- Backend/routes/dashboardRoutes.js
+++ Backend/routes/dashboardRoutes.js
@@ -1,35 +1,21 @@
 import express from "express";
-
 import { protect } from "../middleware/authMiddleware.js";
-
 import {
-
-getNavbar,
-
-getDashboardStats,
-
-getWallet,
-
-getRecentOrders,
-
-getOffers,
-
-getPopularFoods
-
+  getNavbar,
+  getDashboardStats,
+  getWallet,
+  getRecentOrders,
+  getOffers,
+  getPopularFoods
 } from "../controllers/dashboardController.js";
 
 const router = express.Router();
 
-router.get("/navbar",protect,getNavbar);
-
-router.get("/stats",protect,getDashboardStats);
-
-router.get("/wallet",protect,getWallet);
-
-router.get("/orders",protect,getRecentOrders);
-
-router.get("/offers",protect,getOffers);
-
-router.get("/popular-foods",protect,getPopularFoods);
+router.get("/navbar", protect, getNavbar);
+router.get("/stats", protect, getDashboardStats);
+router.get("/wallet", protect, getWallet);
+router.get("/orders", protect, getRecentOrders);
+router.get("/offers", protect, getOffers);
+router.get("/popular-foods", protect, getPopularFoods);
 
 export default router;

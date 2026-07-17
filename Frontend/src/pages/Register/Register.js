774a07493f3e4fe283272135d3385f6f1b9afe16
Yeswanth Varma <your_email@example.com>
2026-07-17 16:07:26 +0530

    Added new customer dashboard and updated login flow 8

--- Frontend/src/pages/Register/Register.js
+++ Frontend/src/pages/Register/Register.js
@@ -195,7 +195,7 @@ const Register = () => {
             
             alert("Registration Successful");
             
-            navigate("/customer");
+            navigate("/login");
     
         }
     

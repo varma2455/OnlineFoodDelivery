49c1186a7960bee14e3dd8d0c797c89e1ad0df08
Yeswanth Varma <your_email@example.com>
2026-07-17 23:03:53 +0530

    ADDING VALUES4

--- Backend/server.js
+++ Backend/server.js
@@ -48,7 +48,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/foods", foodRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
-app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/auth/profile", dashboardRoutes);
 
 
 

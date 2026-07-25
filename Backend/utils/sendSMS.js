5c8239e8a6068a60cec3634a6252771c71229493
Yeswanth Varma <your_email@example.com>
2026-07-25 17:54:33 +0530

    Added OTP authentication and backend updates4

--- Backend/utils/sendSMS.js
+++ Backend/utils/sendSMS.js
@@ -1,20 +1,13 @@
-import axios from "axios";
+// import axios from "axios";
 
 const sendSMS = async (phone, otp) => {
-
     try {
-
-        // MSG91 API code will be added in the next step
-
+        // MSG91 API code will be added later
         console.log("Phone :", phone);
         console.log("OTP :", otp);
-
     } catch (error) {
-
         console.log(error);
-
     }
-
 };
 
 export default sendSMS;
